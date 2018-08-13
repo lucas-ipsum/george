fb29f72bbd193891348522a6b581c3c4732f3e7f
Fabian <fabiankoster@Fabians-MacBook-Pro-3.local>
2018-08-13 14:39:33 +0200

    Feedback-Aufruf

--- routes/botman.php
+++ routes/botman.php
@@ -247,10 +247,9 @@ $botman->hears('say_smalltalk_langweilig', 'App\Http\Controllers\Intents_Control
 $botman->hears('say_smalltalk_Willkommen', 'App\Http\Controllers\Intents_Controller@smalltalk_Willkommen') ->middleware($dialogflow);
 
 //$botman->hears('say_feedback_Intent', 'App\Http\Controllers\Intents_Controller@test') ->middleware($dialogflow);
-/*
+
 //################################################################################################################################################
 //Intent: 00 - feedback_Intent
 $botman->hears('say_feedback_Intent', function ($bot) {
   $bot->startConversation(new App\Http\Conversations\Feedback);
 })->middleware($dialogflow);
-*/
